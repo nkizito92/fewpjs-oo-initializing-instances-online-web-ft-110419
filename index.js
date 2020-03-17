997f3a947add4f38995933925d7cfe95623c48d5
Kizito Njoku <nkizito57@gmail.com>
2020-03-17 18:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code here
 class Breakfast {
   constructor (food, drink ) {
-    this.eggs = eggs;
-    this.juice = juice;
+    this.food = food;
+    this.drink = drink;
   }
   
 }
